9dc9f60f77c4fceb08c7adb510f98e80920fe894
Guillaume Nodet <gnodet@gmail.com>
2026-04-03 17:30:12 +0200

    test: exercise incremental build file-path detection

--- components/camel-bean/src/test/java/org/apache/camel/component/bean/BeanInfoTest.java
+++ components/camel-bean/src/test/java/org/apache/camel/component/bean/BeanInfoTest.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// CI test: exercise incremental build file-path detection
 package org.apache.camel.component.bean;
 
 import java.lang.reflect.Method;
